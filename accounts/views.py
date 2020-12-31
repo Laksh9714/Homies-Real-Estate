af76461a44395228aeec544844fc68767f9372b2
Lakshmeesha Shetty <lss180005@utdallas.edu>
2020-12-31 06:20:11 -0600

    Login functionality and authentication done

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib import messages
+from django.contrib import messages,auth
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -8,11 +8,32 @@ from django.contrib.auth.models import User
 def login(request):
     #Login the user
     if request.method == "POST":
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password = password)
+        
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,"Successfully logged in!")
+            return redirect('dashboard')
+
+        else:
+            messages.error(request,"invalid username or password!")
+            return redirect('login')
+
+
     else:
         return render(request,'accounts/login.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request,"You are now logged out!")
+        return redirect('index') 
+
+
+
     return redirect('index')
 
 def register(request):
