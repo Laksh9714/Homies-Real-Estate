b52580dab288aafa87ca62a81d68c787bc2b3dfb
Lakshmeesha Shetty <lss180005@utdallas.edu>
2020-12-31 01:47:13 -0600

    Login and Register UI is added

--- accounts/views.py
+++ accounts/views.py
@@ -4,12 +4,24 @@ from django.shortcuts import render,redirect
 
 
 def login(request):
-    return render(request,'accounts/login.html')
+    #Login the user
+    if request.method == "POST":
+        return
+    else:
+        return render(request,'accounts/login.html')
 
 def logout(request):
     return redirect('index')
 
 def register(request):
+    #Register the user
+    if request.method == "POST":
+        return
+    else:
+        return render(request,'accounts/register.html')
+
+
+
     return render(request,'accounts/register.html')
 
 def dashboard(request):
