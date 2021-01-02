63e4a8942f5fa80a3cb3254e8b25249f33df9eda
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-01-02 02:37:45 -0600

    Project completed and ready for deployment

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages,auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
+
 
 # Create your views here.
 
@@ -88,4 +90,9 @@ def register(request):
     return render(request,'accounts/register.html')
 
 def dashboard(request):
-    return render(request,'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context = {
+        'contacts' : user_contacts 
+    }
+    return render(request,'accounts/dashboard.html',context)
