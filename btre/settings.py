9ca3b2d6b00f9376cfbe64635895daef6e6ae7b9
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-01-02 02:20:54 -0600

    Inquiry check for each user

--- btre/settings.py
+++ btre/settings.py
@@ -145,3 +145,9 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+
+# #Email Config
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'lakshmeesha.shetty@gmail.com'
